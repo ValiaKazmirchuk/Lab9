f7b0cfe1e724af54f4afe2e9e83d4c164ad59db6
Валя Казмірчук <valiakazmirchuk9@gmail.com>
2024-11-06 10:32:50 +0200

    Add project files.

---
+++ Lab9/Lab9.cpp
@@ -0,0 +1,31 @@
+﻿// Lab9.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <queue>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	queue<int> q;
+	int n, x, sum = 0;
+	cout << "Введіть розмір черги: ";
+	cin >> n;
+	cout << "Введіть " << n << " цілих чисел:\n";
+	for (int i = 0; i < n; i++) {
+		cin >> x;
+		q.push(x);
+	}
+	while (!q.empty()) {
+		if (q.front() < 0) {
+			sum += q.front();
+		}
+		q.pop();
+	}
+
+	cout << "Сума від'ємних елементів черги: " << sum << endl;
+}
+
